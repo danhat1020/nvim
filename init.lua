f9b630fb0a771d182eb64aeb96ef9a26e86d78c7
Daniel Hatfield <danhat1020@gmail.com>
2025-11-09 20:22:34 +0000

    Updated keybinds

--- init.lua
+++ init.lua
@@ -21,16 +21,14 @@ vim.o.winborder = "single"
 vim.o.showmode = false
 -- keymaps
 vim.g.mapleader = " "
-vim.keymap.set({ "n", "v" }, ";", ":", { noremap = true }) -- swaps ; and : in normal mode
-vim.keymap.set({ "n", "v" }, ":", ";", { noremap = true }) -- swaps ; and : in normal mode
 vim.keymap.set({ "n", "v", "x" }, "<leader>y", '"+y', { silent = true, noremap = true }) -- yank to clipboard
 vim.keymap.set({ "n", "v", "x" }, "<leader>p", '"+p', { silent = true, noremap = true }) -- paste from clipboard
 vim.keymap.set("v", "p", '"_dP', { silent = true, noremap = true }) -- paste without overwriting register
 vim.keymap.set({ "n", "v" }, "x", '"_x', { silent = true, noremap = true }) -- delete character without copying to register
 vim.keymap.set("n", "<C-d>", "<C-d>zz") -- center when jumping down half page
-vim.keymap.set("n", "<C-u>", "<C-u>zz") -- center when jumping up half page
 vim.keymap.set("v", "<S-j>", ":m '>+1<CR>gv=gv", { silent = true, noremap = true }) -- move selected lines down
 vim.keymap.set("v", "<S-k>", ":m '<-2<CR>gv=gv", { silent = true, noremap = true }) -- move selected lines up
+vim.keymap.set("n", "<C-u>", "<C-u>zz") -- center when jumping up half page
 vim.keymap.set("n", "<leader>mx", "<CMD>!chmod +x %<CR>", { silent = true, noremap = true }) -- making a file executable
 vim.keymap.set("n", "<leader>/", "/", { noremap = true }) -- regular search
 -- autocmds
