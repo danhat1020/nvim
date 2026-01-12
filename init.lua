2c75bcae204757a44f61fd52ff770e42ec2526a6
Dan Hatfield <danhat1020@gmail.com>
2026-01-12 15:21:58 +0000

    Added fuzzy finder

--- init.lua
+++ init.lua
@@ -19,7 +19,6 @@ vim.o.guicursor = ""
 -- keymaps
 vim.g.mapleader = " "
 vim.keymap.set({ "i", "v", "x" }, "<C-c>", "<Esc>", { silent = true, noremap = true })   -- use ctrl+c as escape
-vim.keymap.set({ "n", "v", "x" }, ";", ":")                                              -- remove need for shift when entering command mode
 vim.keymap.set({ "n", "v", "x" }, "<leader>y", '"+y', { silent = true, noremap = true }) -- yank to system clipboard
 vim.keymap.set({ "n", "v", "x" }, "<leader>p", '"+p', { silent = true, noremap = true }) -- paste from system clipboard
 -- autocommands
@@ -29,17 +28,12 @@ vim.api.nvim_create_autocmd("TextYankPost", {                                   
         vim.highlight.on_yank()
     end,
 })
-vim.api.nvim_create_autocmd("FileType", { -- remove auto commenting
-    group = vim.api.nvim_create_augroup("no_auto_comment", {}),
-    callback = function()
-        vim.opt_local.formatoptions:remove({ "c", "r", "o" })
-    end,
-})
 -- plugins
 vim.pack.add({
     { src = "https://github.com/vague-theme/vague.nvim" },
     { src = "https://github.com/williamboman/mason.nvim" },
     { src = "https://github.com/stevearc/oil.nvim" },
+    { src = "https://github.com/nvim-mini/mini.pick" },
     { src = "https://github.com/nvim-treesitter/nvim-treesitter",          version = "master" },
     { src = "https://github.com/nvim-tree/nvim-web-devicons" },
     { src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" },
@@ -55,26 +49,33 @@ vim.cmd("hi StatusLine guibg=NONE")
 -- lsp setup (ts_ls, html, cssls)
 require("mason").setup({})
 local servers = { "clangd", "bash-language-server", "marksman", "emmylua_ls" }
-local all = vim.list_extend(vim.list_extend({}, servers), { "ast-grep", "beautysh", "prettierd", "stylua" })
+local all = vim.list_extend(vim.list_extend({}, servers), { "ast-grep", "beautysh", "prettierd", "stylua" }) -- formatters
 for _, server in ipairs(all) do
-    if not require("mason-registry").is_installed(server) then
-        vim.cmd("MasonInstall " .. server)
-    end
+    if not require("mason-registry").is_installed(server) then vim.cmd("MasonInstall " .. server) end
 end
 vim.lsp.enable(servers)
 vim.api.nvim_create_autocmd("LspAttach", {
     group = vim.api.nvim_create_augroup("UserLspConfig", { clear = true }),
-    callback = function()
+    callback = function(args)
         vim.keymap.set("n", "K", vim.lsp.buf.hover, { silent = true, noremap = true })                -- Show documentation (hover)
         vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, { silent = true, noremap = true })      -- [L]SP [F]ormat
         vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, { silent = true, noremap = true })      -- [L]SP [R]ename
         vim.keymap.set("n", "<leader>lc", vim.lsp.buf.code_action, { silent = true, noremap = true }) -- [L]SP see available [C]ode actions
         vim.cmd("set completeopt+=noselect")
+        -- auto format on save
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = args.buf,
+            callback = function() vim.lsp.buf.format { async = false, id = args.data.client_id } end,
+        })
     end,
 })
 -- oil
 require("oil").setup({ view_options = { show_hidden = true } })
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { silent = true, noremap = true })
+-- mini.pick
+require("mini.pick").setup({ window = { config = { anchor = 'NE', row = 0, col = 200, width = 60, height = 20, } } })
+vim.keymap.set("n", "<leader>sf", ":Pick files<CR>", { silent = true, noremap = true })
+vim.keymap.set("n", "<leader>sh", ":Pick help<CR>", { silent = true, noremap = true })
 -- treesitter
 require("nvim-treesitter.configs").setup({
     ensure_installed = { "lua", "c", "markdown", "bash" },
